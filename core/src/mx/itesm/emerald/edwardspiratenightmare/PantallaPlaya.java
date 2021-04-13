982cc2e3ae722d5b77af7c0791902b4c2058d07b
Luis Correa <luis.lacv.98@gmail.com>
2021-04-13 15:18:59 -0500

    Cambio de fondo Nivel1 y font

--- core/src/mx/itesm/emerald/edwardspiratenightmare/PantallaPlaya.java
+++ core/src/mx/itesm/emerald/edwardspiratenightmare/PantallaPlaya.java
@@ -2,35 +2,81 @@ package mx.itesm.emerald.edwardspiratenightmare;
 
 import com.badlogic.gdx.graphics.Texture;
 
+import mx.itesm.emerald.edwardspiratenightmare.utilities.Texto;
+
 public class PantallaPlaya extends Pantalla {
+
     private EdwardsPirateNightmare juego;
-    private Texture fondoPlaya;
+    //Fondo infinito
+    private Texture texturaFPlaya;
+    private float xFondo = 0;
+
+    //Personaje
     private Edward edward;
+
+    //Crear Marcador
+    private int puntos = 0;
+    private Texto texto;
 
     public PantallaPlaya(EdwardsPirateNightmare juego) {
         this.juego = juego;
-        fondoPlaya = new Texture("Pantallas/lvl1.png");
+        //fondoPlaya = new Texture("Pantallas/lvl1.png");
     }
+
     // Crear al personaje principal
     private void crearEdward(){
         Texture texturaEdward = new Texture("sprites/edwardFront.png");
-        edward = new Edward(texturaEdward,0,200);
+        edward = new Edward(texturaEdward,20,50);
 
     }
     @Override
     public void show() {
         crearEdward();
+        crearTexto();
+        crearFondo();
+    }
 
+    private void crearFondo() {
+        texturaFPlaya = new Texture("pantallas/P1.png");
+    }
+
+    private void crearTexto() {
+        texto = new Texto("fonts/pirate.fnt");
     }
 
     @Override
-    public void render(float delta) {
+    public void render(float delta)
+    {
+        //Actualizar
+        actualizar();
+
         batch.begin();
         batch.setProjectionMatrix(camara.combined);
-        batch.draw(fondoPlaya,0,0);
+
+        //Dibuja Fondo
+        batch.draw(texturaFPlaya,xFondo,0);
+        batch.draw(texturaFPlaya,xFondo+texturaFPlaya.getWidth(),0);
+
+        //Dibuja personaje
         edward.render(batch);
+
+        //Dibujar marcador
+        texto.mostrarMensaje(batch,Integer.toString(puntos),0.95f*ANCHO,0.95f*ALTO);
+
         batch.end();
 
+    }
+
+    private void actualizar() {
+        actualizarFondo();
+    }
+
+    private void actualizarFondo() {
+        xFondo -=4;
+        if (xFondo<= -texturaFPlaya.getWidth())
+        {
+            xFondo = 0;
+        }
     }
 
     @Override
@@ -48,3 +94,6 @@ public class PantallaPlaya extends Pantalla {
 
     }
 }
+
+
+
---
+++ core/src/mx/itesm/emerald/edwardspiratenightmare/utilities/Texto.java
@@ -0,0 +1,24 @@
+package mx.itesm.emerald.edwardspiratenightmare.utilities;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Texto {
+    private BitmapFont font;
+
+    public Texto(String archivo) {
+        font = new BitmapFont(Gdx.files.internal(archivo));
+    }
+
+    /*
+    Despliega el texto 'mensaje' en coordenadas centrado en (x,y)
+     */
+    public void mostrarMensaje(SpriteBatch batch, String mensaje, float x, float y) {
+        GlyphLayout glyph = new GlyphLayout();
+        glyph.setText(font, mensaje);
+        float anchoTexto = glyph.width;
+        font.draw(batch, glyph, x-anchoTexto/2, y);
+    }
+}
